b788def6ff5c6e896f46fcb90836a10c27d9a1ce
isl-kobayan <adpa0179@chiba-u.jp>
2016-03-23 21:49:22 +0900

    add install script for caffe

---
+++ install-caffe-deps.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler git
+
